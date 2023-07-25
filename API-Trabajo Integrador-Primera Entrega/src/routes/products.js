9941e6cedee44a7d772da34c35533e1bbf7cd22b
jorgextz <jorgeleo.marcano@gmail.com}>
2023-07-24 23:07:58 -0300

    trabajo integrador 1 api

---
+++ API-Trabajo Integrador-Primera Entrega/src/routes/products.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const multer = require('multer');
+const router = express.Router();
+const controller = require('../controllers/productsControllers');
+
+
+router.get('/listar', controller.listar);
+router.get('/detalle', controller.detalle);
+router.post('/crear', controller.crear);
+
+
+
+module.exports = router;
